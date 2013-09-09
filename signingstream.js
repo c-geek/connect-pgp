ea50cd8e506a079cfaf9beab0dd55829e419f35d
cgeek <cem.moreau@gmail.com>
2013-09-09 11:12:46 +0200

    Automatically escape ----BEGIN PGP messages

--- signingstream.js
+++ signingstream.js
@@ -24,11 +24,14 @@ SigningStream.prototype.sign = function(callback) {
   var that = this;
   process.nextTick(function () {
     var body = that.buffer.replace(/\r\n/g, '\n').replace(/\n/g, '\r\n');
-    var ciphertext = that.openpgp.write_signed_message(that.openpgp.keyring.privateKeys[0].obj, body);
+    var cleaned = body;
+    cleaned = cleaned.replace(/-----BEGIN PGP(.*)-----\r\n/g, 'BEGIN PGP$1\r\n');
+    cleaned = cleaned.replace(/-----END PGP(.*)-----\r\n/g, 'END PGP$1\r\n');
+    var ciphertext = that.openpgp.write_signed_message(that.openpgp.keyring.privateKeys[0].obj, cleaned);
     var ciphertext = ciphertext.substring(ciphertext.lastIndexOf('-----BEGIN PGP SIGNATURE-----'));
     var body = '';
     body += '--' + that.boundary + '\n';
-    body += that.buffer + '\n\n';
+    body += cleaned + '\n\n';
     body += '--' + that.boundary + '\n';
     body += 'Content-Type: application/pgp-signature\n\n';
     body += ciphertext + '\n';
