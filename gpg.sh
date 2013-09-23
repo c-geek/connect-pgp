55197dc59145067322d5696a9037dd8b291050ff
cgeek <cem.moreau@gmail.com>
2013-09-23 19:05:13 +0200

    Make detached signatures

--- gpg.sh
+++ gpg.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gpg --batch --no-default-keyring --secret-keyring $1 -s -a --passphrase-fd 3 3<&0 < <(echo $MESSAGE | sed -e "s/\\\\n/\n/g")
+gpg --batch --no-default-keyring --secret-keyring $1 -b -a --passphrase-fd 3 3<&0 < <(echo $MESSAGE | sed -e "s/\\\\n/\n/g")
